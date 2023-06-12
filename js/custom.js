4ec302751d78768715f90cd5aec886cccfa13055
Seulgi Lee <123173170+simseulnyang@users.noreply.github.com>
2023-06-13 00:42:02 +0900

    Delete custom.js

--- js/custom.js
+++
@@ -1,8 +0,0 @@
-// 화면 축소 시 좌측 상단에 보여지는 내비게이션 이벤트 함수
-// 내비게이션 클릭 시 사이드화면 노출, 한번 더 클릭하면 사이드화면 사라짐
-function menuClick() {
-    let $navBar = document.querySelector('.header-menu');
-    let $moreBtn = document.querySelector('.more-btn');
-    $navBar.classList.toggle('active');
-    $moreBtn.classList.toggle('active');
-}
