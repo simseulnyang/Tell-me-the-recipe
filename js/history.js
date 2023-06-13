4a89bbf46bb3b6b7bdea595719b3e8b92f9033ee
Seulgi Lee <123173170+simseulnyang@users.noreply.github.com>
2023-06-13 11:45:09 +0900

    로컬스토리지에 저장된 데이터 불러오기

--- js/history.js
+++ js/history.js
@@ -1,20 +1,11 @@
+const $historyList = document.querySelector("#historySearchName");
+
 let saveHistory = JSON.parse(localStorage.getItem("saveHistory"));
-const $historyList = document.querySelector("nav");
 
 // history 목록 및 선택 시 contents 보여주기
 export const printHistory = () => {
-    let div = document.createElement("div");
-    div.classList.add("history-searchlist");
-    let ul = document.createElement("ul");
-    ul.setAttribute("id", "historySearchName");
     let li = document.createElement("li");
     li.classList.add("history-searchname");
-    li.innerHTML = saveHistory.userQuestions;
-    let a = document.createElement("a");
-
-    a.append(li);
-    ul.append(a);
-    div.append(ul);
-
-    $historyList.appendChild(div);
+    li.innerText = saveHistory.userquestion;
+    $historyList.appendChild(li);
 };
