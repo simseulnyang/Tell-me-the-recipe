b05cf4f78f228a3b7a84de4e3f29b1409d577342
Seulgi Lee <123173170+simseulnyang@users.noreply.github.com>
2023-06-14 23:43:10 +0900

    addEventListener 사용해서 click시 반응하도록 변경

--- js/drawerMenu.js
+++ js/drawerMenu.js
@@ -1,8 +1,9 @@
 // 화면 축소 시 좌측 상단에 보여지는 hamburger menu icon 이벤트 함수
 // icon 클릭 시 드로워메뉴 화면 노출, 한번 더 클릭하면 메뉴 화면 사라짐
-function menuClick() {
-    let $navBar = document.querySelector('.header-menu');
-    let $moreBtn = document.querySelector('.more-btn');
-    $navBar.classList.toggle('active');
-    $moreBtn.classList.toggle('active');
-}
+const $menuClickBtn = document.querySelector(".more-btn");
+$menuClickBtn.addEventListener("click", (e) => {
+    const $navBar = document.querySelector(".header-menu");
+    const $moreBtn = document.querySelector(".more-btn");
+    $navBar.classList.toggle("active");
+    $moreBtn.classList.toggle("active");
+});
